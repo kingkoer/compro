6bbc8846ba7da946e6211cc6d0aaef6da72ae8e0
kingkoer <2015933188@qq.com>
2021-11-06 07:11:39 +0800

    new repo

--- src/Foo.php
+++ src/Foo.php
@@ -1,8 +1,13 @@
 <?php
 
-namespace kingkoer\mytest;
+namespace Kingkoer\Mytest;
+
 class Foo {
 	public function show() {
-		return "show infomation";
+		return "show infomation because of my name is kingkoer";
+	}
+
+	public function test() {
+		return "this is a new repo";
 	}
 }
